5880e6998cbea1864607eaa1f0a6ef1d3f22360b
Javin Erasmus Clementino <javinpro12@gmail.com>
2025-05-11 22:01:37 +0800

    changing bg color to white

--- frontend/lib/taskpost_page.dart
+++ frontend/lib/taskpost_page.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:jam/colors.dart';
 
 class TaskpostPage extends StatelessWidget {
   const TaskpostPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Task Post Page"));
+    return Container(
+      color: backgroundColor,
+      child: Center(child: Text("Post Task Page")),
+    );
   }
 }
