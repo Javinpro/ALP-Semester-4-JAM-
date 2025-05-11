6c3486f86275c74015c15a3f2dd76c41bb4007d9
Javin Erasmus Clementino <javinpro12@gmail.com>
2025-05-11 22:03:21 +0800

    making font template and sizedbox template

---
+++ frontend/lib/text_template.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:jam/colors.dart';
+
+// Text Section
+const headerblack = TextStyle(
+  fontSize: 18,
+  fontWeight: FontWeight.w700,
+  color: secondaryColor,
+);
+
+const headerred = TextStyle(
+  fontSize: 18,
+  fontWeight: FontWeight.w700,
+  color: redColor,
+);
+
+// Space Section
+const sizedbox1 = SizedBox(height: 20);
+const sizedbox2 = SizedBox(height: 25);
+const sizedbox3 = SizedBox(height: 62);
