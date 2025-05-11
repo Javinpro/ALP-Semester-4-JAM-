4b3b265a8a981583b333bfef37a493c04197819a
Javin Erasmus Clementino <javinpro12@gmail.com>
2025-05-11 22:01:52 +0800

    adding new color template

--- frontend/lib/colors.dart
+++ frontend/lib/colors.dart
@@ -5,5 +5,6 @@ import 'package:flutter/material.dart';
 const primaryColor = Color(0xFFFEC330); // so far, ini warna hanya untuk navbar
 const secondaryColor = Color(0xFF000000);
 const backgroundColor = Color(0xFFFFFFFF);
+const redColor = Color(0xFFCC001E);
 // const backgroundColor = Color(0xFF000000);
 // const textColor = Color(0xFF000000);
