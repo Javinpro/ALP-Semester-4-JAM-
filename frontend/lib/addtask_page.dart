36d2e68f7e5587cc3967aebc0e99aa47c34a9981
AryoKM <148169550+AryoKM@users.noreply.github.com>
2025-05-18 22:10:21 +0800

    calendar attempt (read desc)
    Added feature:
    1. Calendar Widget
    2. added Calendar Widget to dashboard
    3. top AppBar and Notification button for dashboard
    4. added a separated notification page to ease future notification page development
    
    Conveniences:
    - changes unchanged pages color to white for consistency

--- frontend/lib/addtask_page.dart
+++ frontend/lib/addtask_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'colors.dart';
 
 class AddItemPage extends StatelessWidget {
   const AddItemPage({super.key});
@@ -6,7 +7,9 @@ class AddItemPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: backgroundColor,
       appBar: AppBar(
+        backgroundColor: backgroundColor,
         title: const Text('Add Task'),
         leading: BackButton(),
       ),
--- frontend/lib/dashboard_page.dart
+++ frontend/lib/dashboard_page.dart
@@ -1,14 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:jam/colors.dart';
 
+import 'notification_page.dart';
+
+import 'calendar_widget.dart';
+
 class DashboardPage extends StatelessWidget {
   const DashboardPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: backgroundColor,
-      child: Center(child: Text("Dashboard Page")),
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(80),
+        child: AppBar(
+          backgroundColor: backgroundColor,
+          elevation: 0,
+          title: const Text(
+            'Dashboard',
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+              fontSize: 35,
+            ),
+          ),
+          actions: [
+            Container(
+              margin: const EdgeInsets.only(right: 20.0, top: 10.0),
+              child: CircleAvatar(
+                backgroundColor: secondaryColor,
+                radius: 30, // Increased size
+                child: IconButton(
+                  icon: const Icon(Icons.notifications_none, color: backgroundColor),
+                  onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const NotificationPage()),
+                );
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 25, top: 10, right: 20, bottom: 25),
+            child: CalendarWidget(),
+          ),
+          // You can add more widgets below if needed
+        ],
+      ),
     );
   }
 }
